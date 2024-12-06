90c6310950302db794e3c9793c9962c40bb7d2fb
Maryam Khan <maryam.dev@outlook.com>
2024-12-05 18:37:00 -0800

    Initialize Spring Boot app

---
+++ src/main/java/com/nestrr/apps/flock/NestrrFlockApplication.java
@@ -0,0 +1,12 @@
+package com.nestrr.apps.flock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestrrFlockApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(NestrrFlockApplication.class, args);
+  }
+}
