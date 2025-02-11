d4d06cd58b3769b9c558755a0b6eb60efddb99d4
Maryam Khan <maryam.dev@outlook.com>
2025-02-11 11:30:44 -0800

    Leave profile update and deletion code out

--- src/main/java/com/nestrr/apps/flock/profile/service/ProfileFacadeService.java
+++ src/main/java/com/nestrr/apps/flock/profile/service/ProfileFacadeService.java
@@ -2,14 +2,9 @@ package com.nestrr.apps.flock.profile.service;
 
 import com.nestrr.apps.flock.profile.dto.OidcProfileRequest;
 import com.nestrr.apps.flock.profile.dto.ProfileDto;
-import com.nestrr.apps.flock.profile.dto.UpdateProfileRequest;
 import org.springframework.security.core.Authentication;
 
 public interface ProfileFacadeService {
 
   ProfileDto getOrCreateProfile(Authentication a, OidcProfileRequest oidcProfileRequest);
-
-  void updateProfile(Authentication a, UpdateProfileRequest updateProfileRequest);
-
-  void deleteProfile(Authentication a);
 }
--- src/main/java/com/nestrr/apps/flock/profile/service/ProfileFacadeServiceImpl.java
+++ src/main/java/com/nestrr/apps/flock/profile/service/ProfileFacadeServiceImpl.java
@@ -5,7 +5,6 @@ import static com.nestrr.apps.flock.util.AuthenticationUtil.getRoles;
 
 import com.nestrr.apps.flock.profile.dto.OidcProfileRequest;
 import com.nestrr.apps.flock.profile.dto.ProfileDto;
-import com.nestrr.apps.flock.profile.dto.UpdateProfileRequest;
 import com.nestrr.apps.flock.profile.entity.Person;
 import com.nestrr.apps.flock.profile.mapper.ProfileMapper;
 import java.util.List;
@@ -50,29 +49,5 @@ public class ProfileFacadeServiceImpl implements ProfileFacadeService {
         .major("")
         .standing("")
         .build();
-  }
-
-  @Override
-  @Transactional
-  public void updateProfile(Authentication a, UpdateProfileRequest updateProfileRequest) {
-    String id = getJwtId(a);
-    if (updateProfileRequest.getName() != null
-        || updateProfileRequest.getProfilePicture() == null
-        || updateProfileRequest.getBio() == null) {
-      personService.updatePerson(
-          id,
-          updateProfileRequest.getName(),
-          updateProfileRequest.getProfilePicture(),
-          updateProfileRequest.getBio());
-    }
-    if (updateProfileRequest.getCampusChoices() != null) {
-      campusChoiceService.updateCampusChoices(id, updateProfileRequest.getCampusChoices());
-    }
-  }
-
-  @Transactional
-  public void deleteProfile(Authentication a) {
-    String id = getJwtId(a);
-    personService.deletePerson(id);
   }
 }
