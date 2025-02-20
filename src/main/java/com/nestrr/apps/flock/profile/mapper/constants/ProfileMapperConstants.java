2597eb04fb98aa2cf8665581fc5f42f6fac25d76
Maryam Khan <maryam.dev@outlook.com>
2025-02-20 01:02:16 -0800

    Refactor and expand ProfileFacadeService

---
+++ src/main/java/com/nestrr/apps/flock/profile/mapper/constants/ProfileMapperConstants.java
@@ -0,0 +1,7 @@
+package com.nestrr.apps.flock.profile.mapper.constants;
+
+public class ProfileMapperConstants {
+  public static final String[] DAYS = {
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  };
+}
--- src/main/java/com/nestrr/apps/flock/profile/service/ProfileFacadeService.java
+++ src/main/java/com/nestrr/apps/flock/profile/service/ProfileFacadeService.java
@@ -2,9 +2,14 @@ package com.nestrr.apps.flock.profile.service;
 
 import com.nestrr.apps.flock.profile.dto.OidcProfileRequest;
 import com.nestrr.apps.flock.profile.dto.ProfileDto;
+import com.nestrr.apps.flock.profile.dto.ProfileUpdateRequest;
 import org.springframework.security.core.Authentication;
 
 public interface ProfileFacadeService {
 
-  ProfileDto getOrCreateProfile(Authentication a, OidcProfileRequest oidcProfileRequest);
+  void storeNewProfile(Authentication a, OidcProfileRequest oidcProfileRequest);
+
+  void updateProfile(Authentication a, ProfileUpdateRequest profileUpdateRequest);
+
+  ProfileDto getProfile(Authentication a);
 }
