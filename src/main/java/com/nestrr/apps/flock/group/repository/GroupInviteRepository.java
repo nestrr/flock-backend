d878fb7bc61705496d354e023ce35ecb804c607a
Maryam Khan <maryam.dev@outlook.com>
2025-03-09 19:29:52 -0700

    Expand repository usage with helper methods

--- src/main/java/com/nestrr/apps/flock/group/repository/GroupInviteRepository.java
+++ src/main/java/com/nestrr/apps/flock/group/repository/GroupInviteRepository.java
@@ -2,9 +2,16 @@ package com.nestrr.apps.flock.group.repository;
 
 import com.nestrr.apps.flock.group.entity.GroupInvite;
 import com.nestrr.apps.flock.group.entity.id.GroupInviteId;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
+import java.util.List;
+
 public interface GroupInviteRepository
     extends ListPagingAndSortingRepository<GroupInvite, GroupInviteId>,
-        ListCrudRepository<GroupInvite, GroupInviteId> {}
+        ListCrudRepository<GroupInvite, GroupInviteId>,
+        JpaRepository<GroupInvite, GroupInviteId> {
+
+  List<GroupInvite> findByIdGroupId(String groupId);
+}
--- src/main/java/com/nestrr/apps/flock/group/repository/GroupStatusRepository.java
+++ src/main/java/com/nestrr/apps/flock/group/repository/GroupStatusRepository.java
@@ -1,9 +1,16 @@
 package com.nestrr.apps.flock.group.repository;
 
 import com.nestrr.apps.flock.group.entity.GroupStatus;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
+import java.util.Optional;
+
 public interface GroupStatusRepository
     extends ListPagingAndSortingRepository<GroupStatus, String>,
-        ListCrudRepository<GroupStatus, String> {}
+        ListCrudRepository<GroupStatus, String> {
+
+  @Query(nativeQuery = true, value = "SELECT * FROM group_status gs WHERE gs.name=?1")
+  Optional<GroupStatus> findByName(String name);
+}
